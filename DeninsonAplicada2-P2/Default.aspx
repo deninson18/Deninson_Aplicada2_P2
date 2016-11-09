5c481d3f02f6d49ba8c99c30df689eb990bef0d2
deninson18 <deninson050895@gmail.com>
2016-11-09 15:05:48 -0400

    Examen Parcial2

---
+++ DeninsonAplicada2-P2/Default.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DeninsonAplicada2_P2.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:LinkButton Class="btn btn-danger"  ID="LinkButton1" runat="server"  OnClick="LinkButton1_Click">LinkButton</asp:LinkButton>
+
+</asp:Content>
---
+++ DeninsonAplicada2-P2/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BLL;
+
+namespace DeninsonAplicada2_P2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            Utility.ShowToastr(this, "Funciona correctamente", "Mensaje", "success");
+
+        }
+    }
+}
